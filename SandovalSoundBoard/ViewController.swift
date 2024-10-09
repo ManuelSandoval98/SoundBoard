c17f1cd7889c3559838f78f06a68af9cdbc3e657
ManuelSandoval98 <julio.sandoval.l@tecsup.edu.pe>
2024-10-09 18:22:44 -0500

    Initial Commit

---
+++ SandovalSoundBoard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SandovalSoundBoard
+//
+//  Created by Manuel Sandoval on 09/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
